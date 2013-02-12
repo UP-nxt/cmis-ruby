e01f9b2bbb32230a56205ce296f6ed500ccb8906
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-12 15:58:59 +0100

    model - folder

--- lib/model/folder.rb
+++ lib/model/folder.rb
@@ -11,18 +11,26 @@ class Folder < Object
 
     def initialize(raw)
         @raw = raw
+        @parent_id = raw['cmis:parentId']
+        @path = raw['cmis:path']
+        @allowed_child_object_type_ids = raw['cmis:allowedChildObjectTypeIds']
     end
 
     def parent
-
+        repository.object(parent_id)
     end
 
     def allowed_child_object_types
-
+        return nil if allowed_child_object_type_ids.nil?
+        allowed_child_object_type_ids.map do |type_id|
+            repository.type(type_id)
+        end
     end
 
     def children
-
+        Services.navigation.get_children(repository_id, object_id, nil, nil, nil, nil, nil, nil, nil, nil).map do |o|
+            Object.create(o)
+        end
     end
 
     def tree(depth)
