49d83241ff3b0472de9d58f6b02aa859f33a54f8
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-12 17:28:12 +0100

    model - server - link with services impl

--- lib/model/server.rb
+++ lib/model/server.rb
@@ -4,13 +4,13 @@ require_relative 'repository'
 module Model
     class Server
         def self.repositories
-            Services.repository.get_repository_infos.map do |repository|
+            Services.repository.get_repository_infos.values.map do |repository|
                 Repository.create(repository)
             end
         end
 
         def self.repository(repository_id)
-            Respository.create(Services.repository.get_repository_info(repository_id))
+            Repository.create(Services.repository.get_repository_info(repository_id).values.first)
         end
     end
 end
