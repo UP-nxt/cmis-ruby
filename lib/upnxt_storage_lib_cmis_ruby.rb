da4bc2286d88b0bcff2904204ece7bf9dd9c99f1
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-13 10:11:53 +0100

    gem - add object services

--- lib/upnxt_storage_lib_cmis_ruby.rb
+++ lib/upnxt_storage_lib_cmis_ruby.rb
@@ -1,5 +1,6 @@
 require 'upnxt_storage_lib_cmis_ruby/repository_services'
+require 'upnxt_storage_lib_cmis_ruby/object_services'
 
 module UpnxtStorageLibCmisRuby
-  VERSION = '0.0.1'
+    VERSION = '0.0.1'
 end
