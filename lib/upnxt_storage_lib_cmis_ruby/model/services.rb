4dfbe0e21dd27b5f7fabdb29fbf7bcd4f073df43
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-14 20:05:14 +0100

    model - services with constructor arg

--- lib/upnxt_storage_lib_cmis_ruby/model/services.rb
+++ lib/upnxt_storage_lib_cmis_ruby/model/services.rb
@@ -3,40 +3,50 @@ require 'upnxt_storage_lib_cmis_ruby'
 module Model
   class Services
 
+    @@url = 'http://localhost:8080/upncmis/browser'
+
+    def self.url
+      @@url
+    end
+
+    def self.url=(url)
+      @@url = url
+    end
+
     def self.repository
-      UpnxtStorageLibCmisRuby::RepositoryServices.new
+      UpnxtStorageLibCmisRuby::RepositoryServices.new url
     end
 
     def self.navigation
-      UpnxtStorageLibCmisRuby::NavigationServices.new
+      UpnxtStorageLibCmisRuby::NavigationServices.new url
     end
 
     def self.object
-      UpnxtStorageLibCmisRuby::ObjectServices.new
+      UpnxtStorageLibCmisRuby::ObjectServices.new url
     end
 
     def self.multi_filing
-      UpnxtStorageLibCmisRuby::MultiFilingServices.new
+      UpnxtStorageLibCmisRuby::MultiFilingServices.new url
     end
 
     def self.discovery
-      UpnxtStorageLibCmisRuby::DiscoveryServices.new
+      UpnxtStorageLibCmisRuby::DiscoveryServices.new url
     end
 
     def self.versioning
-      UpnxtStorageLibCmisRuby::VersioningServices.new
+      UpnxtStorageLibCmisRuby::VersioningServices.new url
     end
 
     def self.relationship
-      UpnxtStorageLibCmisRuby::RelationshipServices.new
+      UpnxtStorageLibCmisRuby::RelationshipServices.new url
     end
 
     def self.policy
-      UpnxtStorageLibCmisRuby::PolicyServices.new
+      UpnxtStorageLibCmisRuby::PolicyServices.new url
     end
 
     def self.acl
-      UpnxtStorageLibCmisRuby::AclServices.new
+      UpnxtStorageLibCmisRuby::AclServices.new url
     end
   end
 end
