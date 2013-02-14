09cbac65e161cceede23a9c83bdddbf003b06da7
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-14 11:24:13 +0100

    model - test - server

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 
 gemspec
+#test
+gem 'rspec', '~> 2.12.0'
---
+++ spec/model/server_spec.rb
@@ -0,0 +1,17 @@
+require 'upnxt_storage_lib_cmis_ruby/model'
+
+describe Model::Server do
+
+  it 'repositories' do
+    Model::Server.repositories.each do |repo|
+      repo.should be_a_kind_of Model::Repository
+    end
+  end
+
+  it 'repository' do
+    id = 'meta'
+    repo = Model::Server.repository(id)
+    repo.should be_a_kind_of Model::Repository
+    repo.id.should.eql? id
+  end
+end
