c556acc0fe214957eebebe14e98fc83aa7e43cd2
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-14 22:16:27 +0100

    test - test repo with relationships, policies & items

--- spec/model/repository_home.rb
+++ spec/model/repository_home.rb
@@ -3,6 +3,9 @@ def create_repository(id)
   f = meta.new_folder
   f.name = id
   f.object_type_id = 'repository'
+  f.properties['supportsRelationships'] = true
+  f.properties['supportsPolicies'] = true
+  f.properties['supportsItem'] = true
   meta.root.create(f)
   Model::Server.repository(id)
 end
