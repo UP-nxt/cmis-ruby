05b0814ede3a38cc9535deb8b1358455d08ee6f7
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-15 09:39:55 +0100

    services - policy

--- lib/upnxt_storage_lib_cmis_ruby/policy_services.rb
+++ lib/upnxt_storage_lib_cmis_ruby/policy_services.rb
@@ -5,14 +5,32 @@ module UpnxtStorageLibCmisRuby
     def initialize(service_url)
       @service = BrowserBindingService.new(service_url)
     end
-    
+
     def apply_policy(repository_id, policy_id, object_id, extension={})
+      params = {
+        cmisaction: 'applyPolicy',
+        policyId: policy_id,
+        objectId: object_id
+      }
+      @service.perform_request("/#{repository_id}", params)
     end
 
     def remove_policy(repository_id, policy_id, object_id, extension={})
+      params = {
+        cmisaction: 'removePolicy',
+        policyId: policy_id,
+        objectId: object_id
+      }
+      @service.perform_request("/#{repository_id}", params)
     end
 
     def get_applied_policies(repository_id, object_id, filter, extension={})
+      params = {
+        cmisselector: 'policies',
+        objectId: object_id,
+        filter: filter
+      }
+      @service.perform_request("/#{repository_id}", params)
     end
   end
 end
