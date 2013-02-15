2ddb06e2e64147b1f2439355b5bc27c925bb1083
Kenneth Geerts <Kenneth.Geerts@up-nxt.com>
2013-02-15 18:46:01 +0100

    Completed DiscoveryServices.

--- lib/upnxt_storage_lib_cmis_ruby/services/discovery_services.rb
+++ lib/upnxt_storage_lib_cmis_ruby/services/discovery_services.rb
@@ -6,27 +6,27 @@ module UpnxtStorageLibCmisRuby
       include Services
 
       def query(repository_id, statement, search_all_versions, include_relationships, rendition_filter, include_allowable_actions, max_items, skip_count, extension={})
-        required = {repositoryId: repository_id,
-                    cmisselector: 'query',
-                    q: statement,
-                    searchAllVersions: search_all_versions,
+        required = {cmisselector: 'query',
+                    repositoryId: repository_id,
+                    q: statement}
+        optional = {searchAllVersions: search_all_versions,
                     includeRelationships: include_relationships,
                     renditionFilter: rendition_filter,
                     includeAllowableActions: include_allowable_actions,
                     maxItems: max_items,
                     skipCount: skip_count}
-        @service.perform_request(required)
+        @service.perform_request(required, optional)
       end
 
       def get_content_changes(repository_id, change_log_token, include_properties, include_policy_ids, include_acl, max_items, extension={})
-        required = {repositoryId: repository_id,
-                    cmisselector: 'contentChanges',
-                    changeLogToken: change_log_token,
+        required = {cmisselector: 'contentChanges',
+                    repositoryId: repository_id}
+        optional = {changeLogToken: change_log_token,
                     includeProperties: include_properties,
                     includePolicyIds: include_policy_ids,
                     includeAcl: include_acl,
                     maxItems: max_items}
-        @service.perform_request(required)
+        @service.perform_request(required, optional)
       end
     end
   end
