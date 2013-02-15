f8862b7f7591f80aaab5e4b74ecc703f1904b17c
Kenneth Geerts <Kenneth.Geerts@up-nxt.com>
2013-02-15 18:53:54 +0100

    ACL Services.

--- lib/upnxt_storage_lib_cmis_ruby/services/acl_services.rb
+++ lib/upnxt_storage_lib_cmis_ruby/services/acl_services.rb
@@ -6,21 +6,21 @@ module UpnxtStorageLibCmisRuby
       include Services
 
       def get_acl(repository_id, object_id, only_basic_permissions, extension={})
-        required = {repositoryId: repository_id,
-                    cmisselector: 'acl',
-                    objectId: object_id,
-                    onlyBasicPermissions: only_basic_permissions}
-        @service.perform_request(required)
+        required = {cmisselector: 'acl',
+                    repositoryId: repository_id,
+                    objectId: object_id}
+        optional = {onlyBasicPermissions: only_basic_permissions}
+        @service.perform_request(required, optional)
       end
 
       def apply_acl(repository_id, object_id, add_aces, remove_aces, acl_propagation, extension={})
-        required = {repositoryId: repository_id,
-                    cmisaction: 'applyACL',
-                    objectId: object_id,
-                    addACEs: add_aces,
+        required = {cmisaction: 'applyACL',
+                    repositoryId: repository_id,
+                    objectId: object_id}
+        optional = {addACEs: add_aces,
                     removeACEs: remove_aces,
                     ACLPropagation: acl_propagation}
-        @service.perform_request(required)
+        @service.perform_request(required, optional)
       end
     end
   end
