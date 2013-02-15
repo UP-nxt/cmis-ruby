a99f39873d5ae1f4aad44ae1e8225aa83be84059
Michael Brackx <michael.brackx@up-nxt.com>
2013-02-15 09:47:39 +0100

    services - multi filing

--- lib/upnxt_storage_lib_cmis_ruby/multi_filing_services.rb
+++ lib/upnxt_storage_lib_cmis_ruby/multi_filing_services.rb
@@ -7,9 +7,22 @@ module UpnxtStorageLibCmisRuby
     end
 
     def add_object_to_folder(repository_id, object_id, folder_id, all_versions, extension={})
+      params = {
+        cmisaction: 'addObjectToFolder',
+        objectId: object_id,
+        folderId: folder_id,
+        allVersions: all_versions
+      }
+      @service.perform_request("/#{repository_id}/root", params)
     end
 
     def remove_object_from_folder(repository_id, object_id, folder_id, extension={})
+      params = {
+        cmisaction: 'removeObjectFromFolder',
+        objectId: object_id,
+        folderId: folder_id
+      }
+      @service.perform_request("/#{repository_id}/root", params)
     end
   end
 end
