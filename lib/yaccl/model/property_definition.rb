3a6a60ea242959840921124d88ef41036211cf07
Kenneth Geerts <Kenneth.Geerts@up-nxt.com>
2013-05-23 18:02:39 +0200

    Added description to property definition.

--- lib/yaccl/model/property_definition.rb
+++ lib/yaccl/model/property_definition.rb
@@ -2,14 +2,16 @@ module YACCL
   module Model
     class PropertyDefinition
 
-      attr_accessor :id, :local_name, :query_name, :inherited,
-        :open_choice, :orderable, :property_type, :updatability,
+      attr_accessor :id, :local_name, :query_name,
+        :description, :inherited, :open_choice,
+        :orderable, :property_type, :updatability,
         :display_name, :required, :cardinality, :queryable
 
       def initialize(hash={})
         @id = hash.delete(:id)
         @local_name = hash.delete(:localName)
         @query_name = hash.delete(:queryName)
+        @description = hash.delete(:description)
         @inherited = hash.delete(:inherited)
         @open_choice = hash.delete(:openChoice)
         @orderable = hash.delete(:orderable)
