09d805ab80871b4e524f4ad065889d77c039cbe8
Kenneth Geerts <kenneth.geerts@up-nxt.com>
2014-03-06 14:33:37 +0100

    Extract req/resp modification in middleware.

--- lib/cmis/connection.rb
+++ lib/cmis/connection.rb
@@ -15,6 +15,7 @@ module CMIS
       conn_opts = { headers: headers, ssl: options[:ssl] }.compact
 
       @http = Faraday.new(conn_opts) do |builder|
+        builder.use RequestModifier
         builder.request :multipart
         builder.request :url_encoded
 
@@ -25,6 +26,7 @@ module CMIS
 
         builder.adapter adapter
         builder.response :logger if options[:log_requests]
+        builder.use ResponseParser
       end
 
       @url_cache = {}
@@ -33,10 +35,8 @@ module CMIS
     def execute!(params = {}, options = {})
       options.symbolize_keys!
 
-      params = massage(params)
       query = options[:query] || {}
       headers = options[:headers]|| {}
-
       url = url(params.delete(:repositoryId), params[:objectId])
 
       response = if params[:cmisaction]
@@ -44,75 +44,102 @@ module CMIS
       else
         @http.get(url, params.merge(query), headers)
       end
-      parse!(response)
+
+      response.body
     end
 
     private
-
-    def massage(hash) # TODO: Extract functionality
-      hash.compact!
-
-      if content_hash = hash[:content]
-        hash[:content] = Faraday::UploadIO.new(content_hash[:stream],
-                                               content_hash[:mime_type],
-                                               content_hash[:filename])
-      end
-
-      if props = hash.delete(:properties)
-        props.each_with_index do |(id, value), index|
-          value = value.to_time if value.is_a?(Date) or value.is_a?(DateTime)
-          value = (value.to_f * 1000).to_i if value.is_a?(Time)
-          if value.is_a?(Array)
-            hash.merge!("propertyId[#{index}]" => id)
-            value.each_with_index do |v, idx|
-              hash.merge!("propertyValue[#{index}][#{idx}]" => value[idx])
-            end
-          else
-            hash.merge!("propertyId[#{index}]" => id,
-                        "propertyValue[#{index}]" => value)
-          end
-        end
-      end
-      hash
-    end
 
     def url(repository_id, object_id)
       if repository_id.nil?
         @service_url
       else
         urls = repository_urls(repository_id)
-        if object_id
-          urls[:root_folder_url]
-        else
-          urls[:repository_url]
-        end
+        urls[object_id ? :root_folder_url : :repository_url]
       end
     end
 
     def repository_urls(repository_id)
       if @url_cache[repository_id].nil?
-        repository_infos = JSON.parse(@http.get(@service_url).body)
+        repository_infos = @http.get(@service_url).body
+
         unless repository_infos.has_key?(repository_id)
           raise Exceptions::ObjectNotFound, "repositoryId: #{repository_id}"
         end
 
         repository_info = repository_infos[repository_id]
-        @url_cache[repository_id] = { repository_url:  repository_info['repositoryUrl'],
-                                      root_folder_url: repository_info['rootFolderUrl'] }
+        @url_cache[repository_id] = {
+          repository_url:  repository_info['repositoryUrl'],
+          root_folder_url: repository_info['rootFolderUrl']
+        }
       end
       @url_cache[repository_id]
     end
+  end
 
-    def parse!(response)
-      if response.headers['Content-Type'] =~ /application\/json/
-        result = JSON.parse(response.body)
-        if result.is_a?(Hash) && ex = result['exception']
-          ruby_exception = "CMIS::Exceptions::#{ex.camelize}".constantize
-          raise ruby_exception, result['message']
+  class RequestModifier < Faraday::Middleware
+    def call(env)
+      if env[:body]
+        env[:body].compact!
+        wrap_content(env)
+        massage_properties(env)
+      end
+      @app.call(env)
+    end
+
+    private
+
+    def wrap_content(env)
+      if content_hash = env[:body][:content]
+        env[:body][:content] = Faraday::UploadIO.new(content_hash[:stream],
+                                                     content_hash[:mime_type],
+                                                     content_hash[:filename])
+      end
+    end
+
+    def massage_properties(env)
+      if props = env[:body].delete(:properties)
+        props.each_with_index do |(id, value), index|
+          if value.is_a?(Array)
+            env[:body][id_key(index)] = id
+            value.each_with_index do |sub_value, sub_index|
+              env[:body][val_key(index, sub_index)] = normalize(sub_value)
+            end
+          else
+            env[:body][id_key(index)] = id
+            env[:body][val_key(index)] = normalize(value)
+          end
         end
-        result.with_indifferent_access
-      else
-        response.body
+      end
+    end
+
+    def normalize(value)
+      value = value.to_time if value.is_a?(Date)
+      value = (value.to_f * 1000).to_i if value.is_a?(Time)
+      value
+    end
+
+    def id_key(index)
+      "propertyId[#{index}]"
+    end
+
+    def val_key(i1, i2 = nil)
+      result = "propertyValue[#{i1}]"
+      result = "#{result}[#{i2}]" if i2
+      result
+    end
+  end
+
+  class ResponseParser < Faraday::Middleware
+    def call(env)
+      @app.call(env).on_complete do |env|
+        if env[:response_headers][:content_type] =~ /\/(x-)?json(;.+?)?$/
+          env[:body] = JSON.parse(env[:body]).with_indifferent_access
+          if env[:body].is_a?(Hash) && ex = env[:body][:exception]
+            ruby_exception = "CMIS::Exceptions::#{ex.camelize}".constantize
+            raise ruby_exception, env[:body]['message']
+          end
+        end
       end
     end
   end
