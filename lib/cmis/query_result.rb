aa3eb6809837906b95d0b16ff369618ef70514c0
Vlad Costea <vlad.costea@up-nxt.com>
2015-04-21 10:18:58 +0300

    default argument for debug_info

--- lib/cmis/query_result.rb
+++ lib/cmis/query_result.rb
@@ -5,7 +5,7 @@ module CMIS
     attr_reader :has_more_items
     attr_reader :debug_info
 
-    def initialize(results, num_items, has_more_items, debug_info)
+    def initialize(results, num_items, has_more_items, debug_info=nil)
       @results = results
       @num_items = num_items
       @has_more_items = has_more_items
