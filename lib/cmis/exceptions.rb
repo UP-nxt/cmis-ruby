8ec60fe2d592faae226fe4154cfdb2ae17533938
Vlad Costea <vlad.costea@up-nxt.com>
2017-11-24 10:50:31 +0200

    Group and order exceptions alphabetically

--- lib/cmis/exceptions.rb
+++ lib/cmis/exceptions.rb
@@ -1,15 +1,16 @@
 module CMIS
   module Exceptions
-    ServiceError                           = Class.new(StandardError)         
+    ServiceError                           = Class.new(StandardError)
+
+    Constraint                             = Class.new(ServiceError)
+    ContentAlreadyExists                   = Class.new(ServiceError)
+    FilterNotValid                         = Class.new(ServiceError)
     InvalidArgument                        = Class.new(ServiceError)
+    NameConstraintViolation                = Class.new(ServiceError)
     NotSupported                           = Class.new(ServiceError)
     ObjectNotFound                         = Class.new(ServiceError)
     PermissionDenied                       = Class.new(ServiceError)
     Runtime                                = Class.new(ServiceError)
-    Constraint                             = Class.new(ServiceError)
-    ContentAlreadyExists                   = Class.new(ServiceError)
-    FilterNotValid                         = Class.new(ServiceError)
-    NameConstraintViolation                = Class.new(ServiceError)
     Storage                                = Class.new(ServiceError)
     StreamNotSupported                     = Class.new(ServiceError)
     UpdateConflict                         = Class.new(ServiceError)
